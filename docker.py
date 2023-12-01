aa855cdc42ed514a2f16a38d1e17fba1706f1690
Sihan Wang <sihanwang0202@gmail.com>
2023-12-01 00:05:16 -0800

    Store config for everyone on the top of this file; Add some postgres queries. Fix some bugs

--- docker.py
+++ docker.py
@@ -13,13 +13,26 @@ from tqdm import tqdm
 import matplotlib.pyplot as plt
 import numpy as np
 
+postgres_db_name = 'postgresdb'
+postgres_password = 'postgres'
+postgres_user = 'postgres'
+postgres_port = '5439'
+neo4j_username = 'neo4j'
+neo4j_password = 'neopassword'
+mongo_username = 'mongouser'
+mongo_password = 'mongopassword'
+
+# csv_file_path_all = ['/Users/chiuchiu/Desktop/game-recommendation-main/dataset/games.csv', '/Users/chiuchiu/Desktop/game-recommendation-main/dataset/recommendations.csv', '/Users/chiuchiu/Desktop/game-recommendation-main/dataset/users.csv']
+csv_file_path_all = ['./dataset/games.csv', './dataset/recommendations.csv', './dataset/users.csv']
+
 
 def infer_sql_data_type(dtype, max_len=None):
     # Map pandas dtypes to SQL data types
     if "int" in str(dtype):
-        return "TEXT"
+        return "INT"
+    # json里面不能用decimal
     elif "float" in str(dtype):
-        return "TEXT"
+        return "FLOAT"
     elif "datetime" in str(dtype):
         return "DATETIME"
     elif "bool" in str(dtype):
@@ -53,7 +66,6 @@ def generate_create_table_query(csv_file, table_name):
 
     fields_str = ", ".join(sql_fields)
     query = f"CREATE TABLE IF NOT EXISTS {table_name} ({fields_str});"
-    
     return query
 
 
@@ -61,8 +73,8 @@ def generate_create_table_query(csv_file, table_name):
 def import_data_to_postgres(csv_file_path, create_table_sql, pg_conn, pg_cursor):
 
     # Execute the SQL statement to create the table
-    pg_cursor.execute("SET search_path TO test")
-    pg_conn.commit()
+    # pg_cursor.execute("SET search_path TO test")
+    # pg_conn.commit()
     pg_cursor.execute(create_table_sql)
     pg_conn.commit()
     f=pd.read_csv(csv_file_path)
@@ -81,9 +93,13 @@ def import_data_to_postgres(csv_file_path, create_table_sql, pg_conn, pg_cursor)
 
 # Define your SQL queries as strings
 queries = [
-    "SELECT * FROM games;",
+    "select title from games where mac=FALSE;",
+    "select g.app_id, g.title from games as g join recommendations r on g.app_id = r.app_id where r.helpful > 0",
+    "select u.user_id from users as u join recommendations r on u.user_id = r.user_id where u.products > 10",
+    "select g.title as recommended_games from"
+    "(select r.app_id from users as u join recommendations r on u.user_id = r.user_id where u.products > 5 and u.reviews > 1 and r.is_recommended = TRUE) as hot_users "
+    "join games g on g.app_id = hot_users.app_id "
 ]
-
 
 def query_launcher(query, redis_conn, pg_cursor):
     redis_key = 'query_result:' + query
@@ -105,86 +121,67 @@ def query_launcher(query, redis_conn, pg_cursor):
 
 def import_data_to_neo4j(driver):
     with driver.session() as session:
-        # Import User Data
-        session.run("LOAD CSV WITH HEADERS FROM 'file:///var/lib/neo4j/import/users_DEMO.csv' AS line "
+        # 导入用户数据
+        session.run("LOAD CSV WITH HEADERS FROM 'file:///users.csv' AS line "
                     "CREATE (:User {userId: line.user_id, products: toInteger(line.products), reviews: toInteger(line.reviews)})")
+        print('users file imported')
 
-        # Import Game Data
-        session.run("LOAD CSV WITH HEADERS FROM 'file:///var/lib/neo4j/import/games.csv' AS line "
+        # 导入游戏数据
+        session.run("LOAD CSV WITH HEADERS FROM 'file:///games.csv' AS line "
                     "CREATE (:Game {appId: line.app_id, title: line.title})")
+        print('games file imported')
 
-        # Import Recommendation Relationships
-        session.run("LOAD CSV WITH HEADERS FROM 'file:///var/lib/neo4j/import/recommendations_DEMO.csv' AS line "
+        # 导入推荐关系
+        session.run("LOAD CSV WITH HEADERS FROM 'file:///recommendations.csv' AS line "
                     "MATCH (user:User {userId: line.user_id}) "
                     "MATCH (game:Game {appId: line.app_id}) "
                     "MERGE (user)-[:RECOMMENDS {hours: toFloat(line.hours), date: line.date, isRecommended: line.is_recommended = 'True'}]->(game)")
+        print('recommendations file matched and imported')
 
-# Example usage for Neo4j
-def print_greeting(session, message):
-    result = session.run("CREATE (a:Greeting) "
-                         "SET a.message = $message "
-                         "RETURN a.message + ', from node ' + id(a)", message=message)
-    return result.single()[0]
+        # # Import User Data
+        # session.run("LOAD CSV WITH HEADERS FROM 'file:///var/lib/neo4j/import/users.csv' AS line "
+        #             "CREATE (:User {userId: line.user_id, products: toInteger(line.products), reviews: toInteger(line.reviews)})")
+        # # Import Game Data
+        # session.run("LOAD CSV WITH HEADERS FROM 'file:///var/lib/neo4j/import/games.csv' AS line "
+        #             "CREATE (:Game {appId: line.app_id, title: line.title})")
+        # # Import Recommendation Relationships
+        # session.run("LOAD CSV WITH HEADERS FROM 'file:///var/lib/neo4j/import/recommendations.csv' AS line "
+        #             "MATCH (user:User {userId: line.user_id}) "
+        #             "MATCH (game:Game {appId: line.app_id}) "
+        #             "MERGE (user)-[:RECOMMENDS {hours: toFloat(line.hours), date: line.date, isRecommended: line.is_recommended = 'True'}]->(game)")
+
 
 
 # Main script execution
 if __name__ == "__main__":
-    csv_file_path_all = ['/Users/chiuchiu/Desktop/game-recommendation-main/dataset/games.csv', '/Users/chiuchiu/Desktop/game-recommendation-main/dataset/recommendations.csv', '/Users/chiuchiu/Desktop/game-recommendation-main/dataset/users.csv']
-    
+
+    ### IMPORT DATA TO DIFFERENT DATABASES
     redis_conn = redis.StrictRedis(host='localhost', port=6379, db=0)
 
     pg_conn = psycopg2.connect(
-        dbname="final_db",
-        user="postgres",
-        password="",
+        dbname=postgres_db_name,
+        user=postgres_user,
+        password=postgres_password,
         host="localhost",
-        port="5432"
+        port=postgres_port
     )
     pg_cursor = pg_conn.cursor()
 
+    # for csv_file_path in tqdm(csv_file_path_all):
+    #     create_table_sql=generate_create_table_query(csv_file_path, csv_file_path.split('/')[-1].split('.')[0])
+    #     import_data_to_postgres(csv_file_path, create_table_sql, pg_conn, pg_cursor)
+    # print('postgres successfully executed.')
 
-
-   
-        #import_data_to_redis(csv_file_path, redis_conn)
-        #print('redis successfully executed.')
-    for csv_file_path in csv_file_path_all:
-
-        create_table_sql=generate_create_table_query( csv_file_path, csv_file_path.split('/')[-1].split('.')[0] )     
-
-        import_data_to_postgres(csv_file_path, create_table_sql, pg_conn, pg_cursor)
-        print('postgres successfully executed.')
-        # Take one of the queries and adjust the order of predicates
-        # Implement additional caching logic as needed
-
-
-    result, source = query_launcher(queries[0], redis_conn, pg_cursor)
-
-
-
-
-        # Connect to Neo4j
-    neo4j_password = ''  # The password you set in docker-compose.yml
     neo4j_uri = "bolt://localhost:7687"
-    neo4j_driver = GraphDatabase.driver(neo4j_uri, auth=("neo4j", neo4j_password))
+    neo4j_driver = GraphDatabase.driver(neo4j_uri, auth=(neo4j_username, neo4j_password))
+    # import_data_to_neo4j(neo4j_driver)
 
-    users_fp = './users.csv'
-    recommendations_fp = './recommendations.csv'
-    games_fp = './games.csv'
+    mongo_conn = pymongo.MongoClient(f"mongodb://{mongo_username}:{mongo_password}@localhost:27017/")
+    mongo_db = mongo_conn["mydatabase"]
 
-    import_data_to_neo4j(neo4j_driver)
-
-
-        # Connect to MongoDB
-    #mongo_username = 'mongouser'  # The username you set in docker-compose.yml
-    #mongo_password = 'mongopassword'  # The password you set in docker-compose.yml
-    #mongo_conn = pymongo.MongoClient(f"mongodb://{mongo_username}:{mongo_password}@localhost:27017/")
-    #mongo_db = mongo_conn["mydatabase"]
-
-
-    # with neo4j_driver.session() as session:
-    #     greeting = print_greeting(session, "hello, world")
-    #     print(greeting)
-
+    ### LAUNCH POSTGRES QUERIES
+    for query in tqdm(queries):
+        result, source = query_launcher(query, redis_conn, pg_cursor)
 
 
     # try:
